318dab621158bbd9da3f2bbabcd2188d3c4daf10
Dan Kokotov <dkokotov@gmail.com>
2024-05-09 10:58:16 -0700

    Include definitions for Json types in the generated model file

--- src/helpers/generateFile.ts
+++ src/helpers/generateFile.ts
@@ -25,7 +25,15 @@ export const generateFile = (
 export type Generated<T> = T extends ColumnType<infer S, infer I, infer U>
   ? ColumnType<S, I | undefined, U>
   : ColumnType<T, T | undefined, T>;
-export type Timestamp = ColumnType<Date, Date | string, Date | string>;`;
+export type Timestamp = ColumnType<Date, Date | string, Date | string>;
+
+export type JsonArray = JsonValue[];
+export type JsonObject = {
+  [K in string]?: JsonValue;
+};
+export type JsonPrimitive = boolean | number | string | null;
+export type JsonValue = JsonArray | JsonObject | JsonPrimitive;
+`;
 
   if (withEnumImport) {
     const enumImportStatement = `import type { ${withEnumImport.names.join(
